e19988968a485a9f465edf2ae80c9d68e081be87
Netta <n2ojne00@students.oamk.fi>
2024-08-27 17:05:09 +0300

    button pressed

--- App.js
+++ App.js
@@ -4,11 +4,19 @@ import logo from './assets/favicon.png';
 import UserInfo from './components/userInfo';
 
 export default function App() {
+
+  function buttonPressed() {
+    console.log("button pressed");
+  }
+
+
   return (
     <View style={styles.container}>
       <Image source={logo}/>
       <Text>Toimiiko</Text>
-      <Button title ='PRESS HERE'/>
+
+      <Button title ='PRESS HERE' onPress={buttonPressed}/>
+
       <UserInfo fname={'Netta'} lname={'Ojala'}/>
       <UserInfo fname={'Ukko'} lname={'Pekka'}/>
       
--- components/userInfo.js
+++ components/userInfo.js
@@ -1,3 +1,5 @@
+import { Button, Text, View } from "react-native";
+
 export default function UserInfo ( {fname, lname}) {
     return (
       <View>
