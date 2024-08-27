fa06dae5655a595bea3171a5845bc97d1f303b70
Netta <n2ojne00@students.oamk.fi>
2024-08-27 18:07:11 +0300

    comments to typing + logo styling and pressed

--- App.js
+++ App.js
@@ -1,27 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, Image, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 import logo from './assets/favicon.png';
 import UserInfo from './components/userInfo';
+import { useState } from 'react';
 
 export default function App() {
 
+  //tilamuuttuja
+  const [name, setName] = useState(''); //can set useState('Netta') to show it when app is opened
+
+    //recognizes input text
     function inputText(text){
       console.log(text)
+      setName(text);
     }
   
   return (
     <View style={styles.container}>
+
+      <Pressable onPress={() => console.log("Logo pressed")} style={{borderWidth: 2, borderColor: "#fd23dd", borderRadius: 30}}>
       <Image source={logo}/>
-      <Text>Toimiiko</Text>
+      </Pressable>
 
-
-      <Button title ='PRESS HERE' onPress={() => console.log("button pressed")}/>
-
+      <Button title ='PRESS HERE' 
+      onPress={() => console.log("button pressed")}
+      color={"#7e80fd"}/>
         <TextInput 
         style={
           {borderWidth: 1, width: 100, padding: 5}}
-          onChangeText={inputText}
+          onChangeText={inputText} //<-- can change inputText as setName
+          //value={name} //Checks set value on name
+          //keyboardType='decimal-pad'  you'll get numberpad
         />
+        <Text>{name}</Text>
+
       <UserInfo fname={'Netta'} lname={'Ojala'}/>
       
      
